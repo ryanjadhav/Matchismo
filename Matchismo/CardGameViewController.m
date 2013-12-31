1cf4a2830ea38971b89fb0f70d871fca0f8f5163
ryanjadhav <ryan.jadhav@gmail.com>
2013-12-30 23:29:14 -0500

    assignemtn 2 controller

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -13,6 +13,8 @@
 @interface CardGameViewController ()
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
 @property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *gameModeControl;
 @property (strong, nonatomic) Deck *deck;
 @property (nonatomic, strong) CardMatchingGame *game;
 @end
@@ -21,8 +23,7 @@
 
 - (CardMatchingGame *)game
 {
-    if (!_game) _game = [[CardMatchingGame alloc] initWithCardCount:[self.cardButtons count]
-                                                          usingDeck:[self createDeck]];
+    if (!_game) _game = [self createGame];
     return _game;
 }
 
@@ -31,14 +32,44 @@
     return [[PlayingCardDeck alloc] init];
 }
 
+- (CardMatchingGame *)createGame
+{
+    return [[CardMatchingGame alloc] initWithCardCount:[self.cardButtons count]
+                                             usingDeck:[self createDeck]];
+}
+
+- (IBAction)resetGame:(UIButton *)sender
+{
+    _game = [self createGame];
+    [self.game resetScore];
+    [self updateUI];
+    self.gameModeControl.enabled = YES;
+}
+
+- (IBAction)setCardMatch:(UISegmentedControl *)sender
+{
+    [self reset];
+
+    UISegmentedControl *segmentedControl = (UISegmentedControl *)sender;
+    NSInteger segmentIndex = [segmentedControl selectedSegmentIndex];
+    if (segmentIndex == 0) {
+        [self.game setNumberOfCardsToTwo];
+    } else if (segmentIndex == 1) {
+        [self.game setNumberOfCardsToThree];
+    }
+    
+    [self updateUI];
+}
+
 - (IBAction)touchCardButton:(UIButton *)sender
 {
     int cardIndex = [self.cardButtons indexOfObject:sender];
     [self.game chooseCardAtIndex:cardIndex];
     [self updateUI];
+    self.gameModeControl.enabled = NO;
 }
 
-- (void) updateUI
+- (void)updateUI
 {
     for (UIButton *cardButton in self.cardButtons) {
         int cardIndex = [self.cardButtons indexOfObject:cardButton];
@@ -50,6 +81,14 @@
         cardButton.enabled = !card.isMatched;
     }
     self.scoreLabel.text = [NSString stringWithFormat:@"Score: %d", self.game.score];
+    self.statusLabel.text = self.game.statusMessage;
+}
+
+- (void)reset
+{
+    _game = [self createGame];
+    [self.game resetScore];
+    self.gameModeControl.enabled = YES;
 }
 
 - (NSString *)titleForCard:(Card *)card
